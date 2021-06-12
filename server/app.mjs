62cf5fefd4737238ff6b14956e5348d00e7f909e
Kou TNK <heikou4522161@gmail.com>
2021-06-12 22:13:01 +0900

    I have modified app.mjs.

--- server/app.mjs
+++ server/app.mjs
@@ -2,27 +2,28 @@ import express from 'express'
 import http from 'http'
 import { Server } from "socket.io"
 import os from 'os'
-
+import path from "node:path"
 import address from '../modules/address.mjs'
 import client from '../modules/client.mjs'
 
 const app = express()
 const httpServer = http.Server(app)
-const mainPath = '/Users/nita14/programming/javascript/murakami/chat_app'
-const chatPath = `${mainPath}/client/index.html`
+const IPv4 = address(os)
+const port = 8081
+console.log(`http://${IPv4}:${port}/`)
+httpServer.listen(port)
+
+const dirname = path.dirname(new URL(import.meta.url).pathname)
+const gamingChatDir = 'gamingChat'
+const mainPath = dirname.split(gamingChatDir)[0] + gamingChatDir
 
 app.use(express.static(mainPath))
+
+const chatPath = `${mainPath}/client/index.html`
 app.get('/', function (req, res) {
   console.log(req.url)
   res.sendFile(chatPath)
 })
-console.log(import.meta)
 
 const io = new Server(httpServer)
 client(io)
-
-const IPv4 = address(os)
-console.log(IPv4)
-
-const port = 8081
-httpServer.listen(port)
