6bc2d7e683c983438a5edac0c6d8e19924fa6fcc
KouTNK <heikou4522161@gmail.com>
2022-06-14 21:28:37 +0900

    test

--- client/hp/main.mjs
+++ client/hp/main.mjs
@@ -1,7 +1,29 @@
 const socket = io.connect(`http://${location.host}/chat_app/hp`)
 let ChatsetData
 window.addEventListener("load", function () {
+// this.document.cookie = "name=oeschger"
+// this.document.cookie="favorite_food=tripe"
+// console.log(document.cookie)
+// document.cookie = "test1=Hello;max-age=1";
+// document.cookie = "test2=World";
+console.log(this.document.cookie)
+// const cookieValue = document.cookie
+//   .split('; ')
+//   .find(row => row.startsWith('test2'))
+//   .split('=')[1];
+//  alert(cookieValue);
+//  console.log(this.document.cookie)
+// if (document.cookie.replace(/(?:(?:^|.*;\s*)doSomethingOnlyOnce\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+//     alert("Do something here!");
+//     document.cookie = "doSomethingOnlyOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+//   }
+//   console.log(this.document.cookie)
+//   document.cookie = "name=;"
+//   console.log(this.document.cookie)
 
+if (document.cookie.split(';').some((item) => item.trim().startsWith('reader='))) {
+    console.log('The cookie "reader" exists (ES6)')
+}
 })
 
 function addOption(selectTag, text, value) {
